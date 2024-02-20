52275b2ea7ab9c1dae93cd0aa46fcc5490bce9bb
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-02-20 11:24:11 -0300

    Enviar mensagem

--- lib/components/new_message.dart
+++ lib/components/new_message.dart
@@ -1,12 +1,51 @@
+import 'package:chat_app/core/services/auth/auth_service.dart';
+import 'package:chat_app/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class NewMenssage extends StatelessWidget {
+class NewMenssage extends StatefulWidget {
   const NewMenssage({super.key});
 
   @override
+  State<NewMenssage> createState() => _NewMenssageState();
+}
+
+class _NewMenssageState extends State<NewMenssage> {
+  String _message = '';
+  final _messageController = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+
+    if (user != null) {
+      await ChatService().save(_message, user);
+      _messageController.clear();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Nova mensagem"),
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _messageController,
+            onChanged: (msg) => setState(() => _message = msg),
+            decoration: InputDecoration(
+              labelText: "Enviar mensagem...",
+            ),
+            onSubmitted: (_){
+              if(_message.trim().isNotEmpty){
+                _sendMessage();
+              }
+            },
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.send),
+          onPressed: _message.trim().isEmpty ? null : _sendMessage,
+        ),
+      ],
     );
   }
 }
