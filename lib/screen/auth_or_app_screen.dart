5b842f185b123b7f81f234a91713806a7c5c9fdf
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-02-22 13:47:42 -0300

    Inicialização do firebase

--- lib/screen/auth_or_app_screen.dart
+++ lib/screen/auth_or_app_screen.dart
@@ -4,23 +4,35 @@ import 'package:chat_app/screen/auth_screen.dart';
 import 'package:chat_app/screen/chat_screen.dart';
 import 'package:chat_app/screen/loading_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppScreen extends StatelessWidget {
   const AuthOrAppScreen({super.key});
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<ChatUser?>(
-        stream: AuthMockService().userChanges,
-        builder: (ctx, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return LoadingScreen();
-          } else {
-            return snapshot.hasData ? ChatScreen() : AuthScreen();
-          }
-        },
-      ),
+    return FutureBuilder(
+      future: init(context),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return LoadingScreen();
+        } else {
+          return StreamBuilder<ChatUser?>(
+            stream: AuthMockService().userChanges,
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return LoadingScreen();
+              } else {
+                return snapshot.hasData ? ChatScreen() : AuthScreen();
+              }
+            },
+          );
+        }
+      },
     );
   }
 }
