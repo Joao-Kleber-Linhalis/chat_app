e21e3e284cb0b1e161da92eedd050fec5a07e734
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-02-26 17:07:26 -0300

    Remover GoogleService-Info.plist do controle de versão

--- lib/core/services/auth/auth_firebase_service.dart
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:chat_app/core/models/chat_user.dart';
 import 'package:chat_app/core/services/auth/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class AuthFirebaseService implements AuthService {
   static ChatUser? _currentUser;
@@ -38,18 +39,34 @@ class AuthFirebaseService implements AuthService {
 
     if (credential.user == null) return;
 
-    credential.user?.updateDisplayName(name);
-    // credential.user?.updatePhotoURL(photoURL);
+    //Upload da foto
+    final imageName = '${credential.user!.uid}.jpg';
+    final imageURL = await _uploadUserImage(image,imageName);
+    
+    //Att user atributos
+    await credential.user?.updateDisplayName(name);
+    await credential.user?.updatePhotoURL(imageURL);
   }
 
   @override
-  Future<void> login(String email, String password) async {}
+  Future<void> login(String email, String password) async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+  }
 
   @override
   Future<void> logout() async {
     FirebaseAuth.instance.signOut();
   }
 
+  Future<String?> _uploadUserImage(File? image, String imageName) async{
+    if(image == null) return null;
+
+    final storage = FirebaseStorage.instance;
+    final imageRef = storage.ref().child('user_images').child(imageName);
+    await imageRef.putFile(image).whenComplete(() {});
+    return await imageRef.getDownloadURL();
+  }
+ 
   static ChatUser _toChatUser(User user) {
     return ChatUser(
       id: user.uid,
